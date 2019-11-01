df153ecd09a244bad27cfd4ec7dfe2d3030b0e76
naruto1231 <45584425+naruto1231@users.noreply.github.com>
2019-11-01 15:23:00 +0800

    Update write2.php

--- write2.php
+++ write2.php
@@ -1,1 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
+<title>ファイルにフォームの文字列を書き込む</title>
+</head>
+<body>
+<form method="post" action="write2.php">
+<textarea name="contents" cols="40" rows="20"></textarea>
+<input type="submit" value="送信">
+</form>
 
+<?php
+$contents = $_POST['contents'];
+if ($contents) {
+  $fp = fopen('write.txt', 'w');
+  fputs($fp, $contents);
+  fclose($fp);
+  print "書き込み完了しました。";
+}
+?>
+
+</body>
+</html>
